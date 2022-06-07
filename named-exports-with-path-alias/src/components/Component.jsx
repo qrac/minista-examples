2c8bf07f5c55e1601e0f5421abbce9882003cec4
Qrac <qrac.jp@gmail.com>
2022-06-07 16:11:03 +0900

    Add named-exports-with-path-alias

---
+++ named-exports-with-path-alias/src/components/Component.jsx
@@ -0,0 +1,7 @@
+import { CONST_A } from "~/lib/constants"
+
+const Component = () => {
+  return <p>{CONST_A}</p>
+}
+
+export default Component
---
+++ named-exports-with-path-alias/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Component from "~/components/Component"
+
+const PageHome = () => {
+  return (
+    <>
+      <Component />
+    </>
+  )
+}
+
+export default PageHome
